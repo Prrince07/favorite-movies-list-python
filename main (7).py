71a12553c5738f8dd60d6016286b2bd85c1b3f88
Prince Kumar <siharamkumar@gmail.com>
2025-08-05 09:49:34 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,7 @@
+
+movie1 = input("Enter the name of your first favorite movie: ")
+movie2 = input("Enter the name of your second favorite movie: ")
+movie3 = input("Enter the name of your third favorite movie: ")
+favorite_movies = [movie1, movie2, movie3] 
+print("\nYour favorite movies are:")
+print(favorite_movies)
